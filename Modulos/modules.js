1b9032fe2ea2f8d9ac6bb12f3633660fab4568f0
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-11-23 13:54:42 -0500

    Se realiza modificación en función de validar ruta y se agrega verificación de archivo o directorio

--- Modulos/modules.js
+++ Modulos/modules.js
@@ -1,24 +1,31 @@
 const fs = require("fs");
 
-//Comprueba si la ruta es valida. Se usa un modulo de node.js
+// Comprueba si la ruta es valida. Se usa el módulo 'stat' de node.js
 const checkingValidPath = (path) => {
     return new Promise((resolve, reject) => {
         fs.stat(path, (err, stats) => {
-            if (err) {
-                // Si hay un error con la ruta, se muestra mensaje y el tipo de error
+            if (err) { // Si hay un error con la ruta, se muestra mensaje y el tipo de error
                 reject(console.log('La ruta ingresada no es valida, por favor verifique', err.message));
-                /*console.log(`Is file: ${stats.isFile()}`);
-                console.log(`Is directory: ${stats.isDirectory()}`);  */ 
-            } else {
-                /* Si no hay ningún error de resuelve la promesa. De momento se imprime en consola los stats 
-                de la ruta */
-                resolve(console.log(stats));
+            } else if (stats.isDirectory() === true) { // Comprueba si la ruta es un directorio
+                resolve(console.log('Esta ruta es un Directorio'));
+            } else { // Se resuelve la promesa, comprobando que es un archivo.
+                resolve(console.log('Esta ruta es un Archivo'));
             }
         })
     })
-    
 }
-
-checkingValidPath('README.md')
+checkingValidPath('./README.md')
     .then(() => console.log('Todo salio bien'))
     .catch(err => console.error('Aqui el error', err));
+
+
+const verifyingTypePath = (path) => {
+
+        console.log(`Is file: ${stats.isFile()}`);
+        console.log(`Is directory: ${stats.isDirectory()}`);
+}
+
+// verifyingTypePath('README.md');
+
+
+// usar Promise.all para ejecutar todas las promesas en orden y obtener las respuestas de cada una.
