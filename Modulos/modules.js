290baf0aa3d4ee8623743faaf1d9d14ba1b387b7
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-11-23 15:02:19 -0500

    Se crea función que valida si el archivo es .md

--- Modulos/modules.js
+++ Modulos/modules.js
@@ -1,28 +1,34 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
 // Comprueba si la ruta es valida. Se usa el módulo 'stat' de node.js
 const checkingValidPath = (path) => {
     return new Promise((resolve, reject) => {
         fs.stat(path, (err, stats) => {
             if (err) { // Si hay un error con la ruta, se muestra mensaje y el tipo de error
-                reject(console.log('La ruta ingresada no es valida, por favor verifique', err.message));
+                reject(console.log('La ruta ingresada no es valida', err.message));
             } else if (stats.isDirectory() === true) { // Comprueba si la ruta es un directorio
                 resolve(console.log('Esta ruta es un Directorio'));
-            } else { // Se resuelve la promesa, comprobando que es un archivo.
-                resolve(console.log('Esta ruta es un Archivo'));
+            } else { // Se resuelve la promesa, comprobando que es un archivo '.md'
+                resolve(verifyingTypePath(path));
             }
         })
     })
 }
+
 checkingValidPath('./README.md')
     .then(() => console.log('Todo salio bien'))
-    .catch(err => console.error('Aqui el error', err));
+    .catch(err => console.error('Por favor, verifique'));
 
 
-const verifyingTypePath = (path) => {
-
-        console.log(`Is file: ${stats.isFile()}`);
-        console.log(`Is directory: ${stats.isDirectory()}`);
+const verifyingTypePath = (filePath) => {
+    return new Promise((resolve, reject) => {
+        if (path.extname(filePath) === '.md') {
+            resolve(console.log('Este archivo es', path.extname(filePath)));
+        } else  {
+            reject(console.log('Este archivo es', path.extname(filePath), 'no podemos procesarlo'))
+        }
+    })
 }
 
 // verifyingTypePath('README.md');
