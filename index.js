d82898bbec95edabe02e8088a8d6b9e782002b57
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-12-06 09:50:33 -0500

    Se modifica la función de búsqueda de archivos en directorios,  mejorando la calidad del código

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const modulePath = require('path');
 
 //Función MDLinks
-const mdLinks = (path) => {
+const mdLinks = (path, options) => {
   return new Promise((resolve, reject) => {
       
         resolve(checkingValidPath(path));   
@@ -18,7 +18,7 @@ const mdLinks = (path) => {
 const checkingValidPath = (path) => {
     return new Promise((resolve, reject) => {
         fs.stat(path, (err, stats) => {
-            if (err) { // Si hay un error con la ruta, se muestra mensaje y el tipo de error
+            if (err) { // Si hay un error con la ruta, se rechaza la promesa y muestra mensaje al usuario
                 reject(console.log('La ruta ingresada no es valida', err.message));
             } else if (stats.isDirectory() === true) { // Comprueba si la ruta es un directorio
                 resolve(searchTheDirectory(path));
@@ -34,7 +34,7 @@ const verifyingTypePath = (filePath) => {
     return new Promise((resolve, reject) => {
         if (modulePath.extname(filePath) === '.md') { //Comprueba que la extensión de el archivo es '.md'
             resolve(readPath(filePath));// Se resuelve la promesa con la lectura el archivo encontrado
-        } else  { // Informa al usuario el tipo de extensión del archivo y que no podrá ser procesado
+        } else  { // Se rechaza la promesa y se informa al usuario el tipo de extensión del archivo y que no podrá ser procesado
             reject(console.log('Este archivo es', modulePath.extname(filePath), 'no podemos procesarlo'))
         }
     })
@@ -44,17 +44,15 @@ const verifyingTypePath = (filePath) => {
 const searchTheDirectory = (path) => {
     return new Promise((resolve, reject) => {
         fs.readdir(path, (err, files) => { // Entra en el directorio  
-            if (err) { // si hay un error se imprime el error en consola
+            if (err) { // si hay un error se rechaza la promesa y se imprime el error en consola
                 reject(console.log('Este directorio presenta una error:', err.message));
             } else { // recorre los archivos dentro del directorio, buscando los que tengan extensión .md
-                let foundPaths = [];  // creo una variable con un array vacío, para ingresar los archivos .md encontrados
-                files.forEach(file => { // recorro los archivos encontrados
-                    if(modulePath.extname(file) === '.md') { // verifico los que sean extensión .md
-                      const test = modulePath.resolve(path); // se valida la ruta absoluta del archivo
-                      const pathAdsolute = test + '\\' + file;// se une la ruta absoluta con el nombre del archivo
-                      foundPaths.push(pathAdsolute) // lleno mi array con los archivos .md encontrados
-                      resolve(readPath(pathAdsolute)); // Se resuelve la promesa con la lectura el archivo encontrado
-                    } else {// si existe un archivo que no sea .md se imprime en consola mensaje para el usuario.
+                files.map(file => {
+                    if(modulePath.extname(file) === '.md') {
+                        const test = modulePath.resolve(path); // se valida la ruta absoluta del archivo
+                        const pathAdsolute = test + '\\' + file;// se une la ruta absoluta con el nombre del archivo
+                        resolve(readPath(pathAdsolute)); // Se resuelve la promesa con la lectura el archivo encontrado
+                    } else {// si existe un archivo que no sea .md se rechaza la promesa y se imprime en consola mensaje para el usuario.
                         reject(console.log('Este directorio contiene archivos:', file, 'que no pueden ser procesados'));
                     }
                 })
@@ -67,28 +65,29 @@ const searchTheDirectory = (path) => {
 const readPath = (path) => {
     return new Promise((resolve, reject) => {
         fs.readFile(path, 'UTF8', (err, data) => { // se lee la ruta recibida y se convierte.
-            if(err) { // Si hay un error con la ruta, se hace reject de la promesa
+            if(err) { // Si hay un error con la ruta, se rechaza de la promesa
                 reject(console.log('No se pudo leer la ruta, por favor verifique.', err.message))
             } else {
                 const hrefMd = /\[([^\[]+)\](\(.*\))/gm // expresión regular para encontrar los links y sus textos
                 const hrefLinks = data.match(hrefMd) // obtenemos las coincidencias con la expresión regular dentro de la cadena.
                 const singleMatch = /\[([^\[]+)\]\((.*)\)/  
-                let foundLinks = [];  // creo una variable con un array vacío, para ingresar mis objetos
-                    for(let i=0; i < hrefLinks.length; i++) { // creo un ciclo sobre cada link encontrado
-                        const text = singleMatch.exec(hrefLinks[i]) // Busqueda sobre las coincidencias con expresión regular
-                        foundLinks.push({ // Le hago un push de cada objeto a mi array foundLinks
-                            href: text[2],
-                            text: text[1],
-                            file : path
-                        })
+                // recorro cada links encontrado en hrefLinks
+                const links = hrefLinks.map(links => {
+                    const text = singleMatch.exec(links) // Busqueda sobre las coincidencias con expresión regular
+                    const link = { // Creo mi objeto de arrays
+                        href: text[2],
+                        text: text[1],
+                        file : path
                     }
-                resolve(console.log(foundLinks)) // se resuelve la promesa mostrando en consola mi array con objetos
+                    return link
+                })
+                resolve(console.log(links)) // se resuelve la promesa mostrando en consola mi array con objetos
             }   
         })
     })
 }
 
 // './Modulos/' --- 'README.md'
-mdLinks('./Modulos/prueba.md')
+mdLinks('./Modulos/prueba2.md')
     .then(() => console.log('***Proceso exitoso***'))
     .catch(err => console.error('Por favor, verifique'));
